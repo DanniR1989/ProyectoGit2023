9d0854bf95344fbc9928e53442b6440cd20e7d11
Danni Recinos <isaias07rec89@gmail.com>
2023-12-18 09:51:48 -0500

    Estamos agregando un cambio al proyecto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ProyectoGit2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inicio del Proyecto");
+            Console.WriteLine("Proyeccto para la administracion de empleados");
+            Console.WriteLine("------------------MENU-----------------------");
+            Console.WriteLine("\t 1) Agregar Empleado: ");
+            Console.WriteLine("\t 2) Listado de Empleados: ");
+            Console.WriteLine("\t 3) Salir: ");
+        }
+    }
+}
