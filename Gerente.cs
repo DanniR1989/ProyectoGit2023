200459f83b18d47dcbfb2dc91f81aaf19e069693
Danni Recinos <isaias07rec89@gmail.com>
2023-12-22 12:01:06 -0500

    Creacion de empleado

--- Gerente.cs
+++ Gerente.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProyectoGit2023
 {
-    internal class Gerente
+     class Gerente:Empleado
     {
+        private const double BONO_GERENTE = 0.15;
+        public Gerente()
+        {
+
+        }
+        public Gerente(string tipoEmpleado, string nombre, double salario)
+        {
+            Salario = salario;
+            Nombre = nombre;
+            TipoEmpleado = tipoEmpleado;
+        }
+        public override double CalcularSalario()
+        {
+            double BonoIncentivo = Salario * 0.05;
+            double BonoGerente = Salario * BONO_GERENTE;
+            return Salario + BonoIncentivo + BonoGerente;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@
             Console.WriteLine("\t 1) Agregar Empleado: ");
             Console.WriteLine("\t 2) Listado de Empleados: ");
             Console.WriteLine("\t 3) Salir: ");
+            Console.WriteLine("Seleccione una Opcion: ");
+            char Opcion = Char.Parse(Console.ReadLine());
+            switch(Opcion)
+            {
+                case '1':
+                    Console.WriteLine("------------Agregar Empleado--------------");
+                    break;
+                case '2':
+                    Console.WriteLine("------------Listado Empleados--------------");
+                    break;
+                case '3':
+                    Console.WriteLine("------------Saliendo, Adios--------------");
+                    break;
+                default: 
+                    Console.WriteLine("Opcion invalida");
+                    break;
+            }
+
         }
     }
 }
